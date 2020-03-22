efeef814b923d016c3a54b7f8f137d8099931587
liwenjie001 <1554616416@qq.com>
2020-03-23 00:12:53 +0800

    '测试github提交问题'

---
+++ src/main/java/Test.scala
@@ -0,0 +1,10 @@
+/**
+ * @ Author: Mr.Li 
+ * @ Date: 2020-03-23 00:11 
+ * @ Description: 这个是测试一下github 上的提交问题。
+ **/
+object Test {
+  def main(args: Array[String]): Unit = {
+    println()
+  }
+}
